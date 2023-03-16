f0d2d76d54093c44c917aeccb695b554ea5aa4e8
GopikaBalagopal <kvgopikabalagopal@gmail.comm>
2023-03-16 15:22:58 +0530

    task

---
+++ script.js
@@ -0,0 +1,38 @@
+
+    let div=document.createElement("div")
+    div.setAttribute("class","container");
+    let p=document.createElement("div");
+    p.setAttribute("class","row");
+    p.classList.add("row" ,"m-3")
+    div.append(p);
+    
+    fetch("https://restcountries.com/v2/all").then((data)=> data.json()).then((response)=>{
+        Countries(response);
+    }).catch((error)=> console.log(error));
+    
+    function Countries(response){
+        response.forEach(({name,alpha3Code,capital,region,flag,latlng})=>{
+            let countrycode=alpha3Code;
+            if(latlng == undefined){
+                latlng=[0,0];
+            }
+            let lattitude=latlng[0];
+            let longitude=latlng[1];
+            p.innerHTML+=`<div class="col-lg-4 col-sm-12">
+                <div class="card text-white bg-dark mb-3" style="max-width: 15rem;">
+                <h4 class="card-header">${name}</h4>
+                 <img src="${flag}" class="card-img-top">
+                 <div class="card-body">
+                 <h6>Capital: ${capital}</h6>
+                 <h6>Region: ${region}</h6>
+                 <h6>Country Code: ${countrycode}</h6>
+                 <center><a href="https://api.openweathermap.org/data/3.0/onecall?lat=${lattitude}&lon=${longitude}&exclude={part}&appid={fbc46de90dc72232a5f5e4b3062c4710}" class="btn btn-light" target="_blank">Check for Weather</a> </center>
+                 </div>
+               </div>
+                  </div>`
+            document.body.append(p);
+                   });
+    }
+  
+    
+  
